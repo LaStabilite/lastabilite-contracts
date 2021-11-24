51b1c3b9a3532110b81c7e0e1e3b8e2e1252d959
ms-cash <ms.cash@protonmail.com>
2021-11-24 22:24:56 +0000

    Update deployment for cUSD-asUSDC

--- migrations/2_deploy_all.js
+++ migrations/2_deploy_all.js
@@ -4,16 +4,24 @@ const MobiusVault = artifacts.require("MobiusVault");
 
 module.exports = function (deployer, network) {
   if (network === "celo") {
-    deployer.deploy(STABIL);
-    deployer.deploy(Stabilite, "Stabilite USD", "stabilUSD");
+    // deployer.deploy(Stabilite, "Stabilite USD", "stabilUSD");
+    // deployer.deploy(
+    //   MobiusVault,
+    //   "MobLP USDC/cUSD Vault",
+    //   "mobVAULT",
+    //   "0x5f0200ca03196d5b817e2044a0bb0d837e0a7823", // MOBI Minter
+    //   "0xdaa2ab880b7f3d5697e6f85e63c28b9120aa9e07", // LP Gauge
+    //   "0xA5037661989789d0310aC2B796fa78F1B01F195D", // LP Swap
+    //   "0x63479641B608324Bb33A80D2cD2B6Ff42B0A4C08" // RewardsRecipient
+    // );
     deployer.deploy(
       MobiusVault,
-      "MobLP USDC/cUSD Vault",
+      "MobLP cUSD/asUSDC Vault",
       "mobVAULT",
       "0x5f0200ca03196d5b817e2044a0bb0d837e0a7823", // MOBI Minter
-      "0xdaa2ab880b7f3d5697e6f85e63c28b9120aa9e07", // LP Gauge
-      "0xA5037661989789d0310aC2B796fa78F1B01F195D", // LP Swap
-      "0xf1d10a171fF8C269882f132803c64e5dcC12875F" // RewardsRecipient
+      "0x27D9Bfa5F864862BeDC23cFab7e00b6b94488CC6", // LP Gauge
+      "0x63C1914bf00A9b395A2bF89aaDa55A5615A3656e", // LP Swap
+      "0x63479641B608324Bb33A80D2cD2B6Ff42B0A4C08" // RewardsRecipient
     );
   }
 };
